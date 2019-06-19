e3b8f1a2078978002cae014833c208e5d921ea89
Calvin Ardi <calvin@isi.edu>
2019-06-18 18:26:27 -0700

    config: add option for IndexDb, clean up with gofmt

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,22 +2,16 @@ package config
 
 import (
 	"encoding/json"
-    "log"
+	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-/*type DataSource struct {
-	Paths   []string
-	Include []string
-	Exclude []string
-	Type    string
-	Alias   []string
-}//*/
-
 type Configuration struct {
-    Name     string
+	Name     string
+	IndexDb  string
 	IndexDir string
 	Paths    []string
 	Include  []string
@@ -28,29 +22,41 @@ type Configuration struct {
 
 func NewConfiguration(path string) (*Configuration, error) {
 
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
 	config := Configuration{}
 
-    decoder := json.NewDecoder(file)
+	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&config)
 	if err != nil {
 		return nil, err
 	}
 
-    // Save the base name of the config file as this configs name.
-    config_parts := strings.Split(path, "/")
-    config.Name = strings.Split(config_parts[len(config_parts)-1], ".")[0]
+	// Save the base name of the config file as this configs name.
+	config_parts := strings.Split(path, "/")
+	config.Name = strings.Split(config_parts[len(config_parts)-1], ".")[0]
 
-    // Normalized all the involved directories to absolute paths
-    config.IndexDir, _ = filepath.Abs(config.IndexDir)
-    for i, path := range config.Paths {
-        config.Paths[i],_ = filepath.Abs(path)
-    }
+	// Check that only one type (DB or CSV) is specified
+	if config.IndexDb != "" && config.IndexDir != "" {
+		return nil, fmt.Errorf("configuration file must specify either IndexDb or IndexDir, but not both.")
+	}
+
+	// Normalized all the involved directories to absolute paths
+	if config.IndexDb != "" {
+		config.IndexDb, _ = filepath.Abs(config.IndexDb)
+	}
+
+	if config.IndexDir != "" {
+		config.IndexDir, _ = filepath.Abs(config.IndexDir)
+	}
+
+	for i, path := range config.Paths {
+		config.Paths[i], _ = filepath.Abs(path)
+	}
 
 	return &config, nil
 }
@@ -65,26 +71,28 @@ func NewConfiguration(path string) (*Configuration, error) {
 //      matched the include pattern
 //
 func (cfg *Configuration) Match(path string) bool {
-    for _, exclude_pattern := range cfg.Exclude {
-        match, err := filepath.Match(exclude_pattern, path)
-        if err != nil {
-            log.Printf("Bad exclude pattern: %s", exclude_pattern)
-            return false
-        }
-        if match {
-            return false
-        }
-    }
+	for _, exclude_pattern := range cfg.Exclude {
+		match, err := filepath.Match(exclude_pattern, path)
+		if err != nil {
+			log.Printf("Bad exclude pattern: %s", exclude_pattern)
+			return false
+		}
+		if match {
+			return false
+		}
+	}
 
-    for _, include_pattern := range cfg.Include {
-        match, err := filepath.Match(include_pattern, path)
-        if err != nil {
-            log.Printf("Bad include pattern: %s", include_pattern)
-            return false
-        }
-        if match {
-            return true
-        }
-    }
-    return false
+	for _, include_pattern := range cfg.Include {
+		match, err := filepath.Match(include_pattern, path)
+		if err != nil {
+			log.Printf("Bad include pattern: %s", include_pattern)
+			return false
+		}
+		if match {
+			return true
+		}
+	}
+	return false
 }
+
+// vim: noet:ts=4:sw=4:tw=80
