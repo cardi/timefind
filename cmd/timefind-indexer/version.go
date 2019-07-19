34eedb2271489d0903bc513480d7c7de482ef83b
Calvin Ardi <calvin@isi.edu>
2019-07-18 17:04:14 -0700

    go: add GPL2+ license

--- cmd/timefind-indexer/version.go
+++ cmd/timefind-indexer/version.go
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 package main
 
 const IndexerVersion = "1.0.4.1"
--- cmd/timefind/version.go
+++ cmd/timefind/version.go
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 package main
 
 const TimefindVersion = "1.0.4.1"
